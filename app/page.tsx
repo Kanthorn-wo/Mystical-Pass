ec570e0457e224824a571f66773136bf749f64a4
Kanthorn1995 <kanthorn.wo@rmuti.ac.th>
2025-09-24 04:51:59 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,9 @@ export default function Home() {
           {/* แสดง PricePerDayList ที่บรรทัด 29 */}
           หน้าแรก
         </div>
+        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
+          <Link href={"/protected"}>ไป Dashboard</Link>
+        </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <p>
